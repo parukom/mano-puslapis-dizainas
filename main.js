15bd4ae549d2fbd1d93624c4b63d2d2b38dae734
Parukom <tomasdudovicius@gmail.com>
2022-03-07 08:35:31 +0200

    bandau suprast ar veiokia pushinimas

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+
+//// Viskas kas liecia navigacija /////
+
 const burger = document.querySelector('#burger');
 const navMenu = document.querySelector('.navMenu');
+const barToX = document.querySelectorAll('.bar');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     navMenu.classList.toggle('active')
+    barToX.classList.toggle('active')
 })
 
 document.querySelectorAll('linkai').forEach(n = n
@@ -11,3 +16,4 @@ document.querySelectorAll('linkai').forEach(n = n
         burger.classList.remove('active')
         navMenu.classList.remove('active')
     }))
+///// Viskas kas liecia navigacija /////
