10bfd6dc5aa7884d99929ef58a233c4a1dad1373
Parukom <tomasdudovicius@gmail.com>
2022-03-07 13:41:13 +0200

    bandau commitint

--- main.js
+++ main.js
@@ -3,17 +3,15 @@
 
 const burger = document.querySelector('#burger');
 const navMenu = document.querySelector('.navMenu');
-const barToX = document.querySelectorAll('.bar');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     navMenu.classList.toggle('active')
-    barToX.classList.toggle('active')
-})
+});
 
-document.querySelectorAll('linkai').forEach(n = n
+document.querySelectorAll('linkai').forEach(e = e
     .addEventListener('click', () => {
         burger.classList.remove('active')
         navMenu.classList.remove('active')
-    }))
+    }));
 ///// Viskas kas liecia navigacija /////
